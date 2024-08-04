3235cb3a5590f4aacd298e5da1ee2c2f093068c7
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-08-04 18:27:30 -0300

    feat: adicionar permissões

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +44,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function assignPermission(string $permission): void
+    {
+
+        $permission = $this->permissions()->firstOrCreate([
+            'name' => $permission,
+        ]);
+
+        $this->permissions()->attach($permission);
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return $this->permissions()->where('name', $permission)->exists();
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,22 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        Gate::define('super-admin', function (User $user) {
+            return $user->hasPermission('super-admin');
+        });
+
+        Gate::define('admin', function (User $user) {
+            return $user->hasPermission('admin');
+        });
+
+        Gate::define('scan-admin', function (User $user) {
+            return $user->hasPermission('scan-admin');
+        });
+
+        Gate::define('user', function (User $user) {
+            return $user->hasPermission('user');
+        });
+
     }
 }
