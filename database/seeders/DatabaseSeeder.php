4f205e3d08af778cccb93cc331e6c0fc1c50d68d
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2024-08-04 18:30:29 -0300

    feat: criar model/migration/factory de `genres`

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'super-admin@ninjaga.com',
         ]);
+        $user->assignPermission('super-admin');
+
+        Genre::factory(20)->create();
     }
 }
